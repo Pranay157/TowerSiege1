5c3596873c895adaed20ce7149f21edbca33e55f
Pranay157 <74853259+Pranay157@users.noreply.github.com>
2021-05-09 13:33:56 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,17 @@
+class Block {
+    constructor(x,y,width,height)
+    {
+        var options={
+            restitution:1
+            
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.body)
+    }
+    display(){
+        var pos=this.body.position;
+        rectMode(CENTER)
+        stroke("red")
+        rect(pos.x,pos.y,this.width,this.height)
+    }
+}
